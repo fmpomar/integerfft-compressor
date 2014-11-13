51d53357eef2ca3a4bd08d7f7edee498fe6ddcb0
Federico Pomar <fpomar@users.noreply.github.com>
2014-11-13 11:45:30 -0300

    Added ratio to information

--- main.py
+++ main.py
@@ -10,13 +10,16 @@ if (len(sys.argv) < 2):
 
 m = numpy.asmatrix(PIL.Image.open(sys.argv[1]))
 print "Estimating Huffman Size ... "
+size_before = huffman.estimate(m.A1)/8 + 1
 print "Estimated Huffman Size:", \
-    huffman.estimate(m.A1)/8 + 1, "bytes"
+    size_before, "bytes"
 print "Transforming matrix ..."
 transformed_m = matrix.dct(m)
 print "Done. Estimating Transformed Huffman Size ..."
+size_after = huffman.estimate(transformed_m.A1)/8 + 1
 print "Estimated Transformed Huffman Size:", \
-    huffman.estimate(transformed_m.A1)/8 + 1, "bytes"
+    size_after, "bytes"
+print "Compression ratio:", float(size_after)/float(size_before)
 print "Inverting Transformation ..."
 untransformed_m = matrix.inverse_dct(transformed_m)
 if numpy.array_equal(untransformed_m, m):
